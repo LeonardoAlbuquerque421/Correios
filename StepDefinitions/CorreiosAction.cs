31c87b897436fef778f1144a8d008043381714dc
leonardo.albuquerque <lalbuquerque.digisystem@alelo.com.br>
2023-05-26 13:11:52 -0300

    finalizado

---
+++ StepDefinitions/CorreiosAction.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Buffers.Text;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using TechTalk.SpecFlow.Analytics;
+
+namespace CorreiosSpecFlow.Actions
+{
+    public class CorreiosAction
+    {
+        private IWebDriver driver;
+
+        public CorreiosAction(IWebDriver driver)
+        {
+            this.driver = driver;
+        }
+
+        public void clickCookieSiteCorreiosAction()
+        {
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+
+            wait.Until((drv) => driver.FindElement(By.Id("btnCookie")).Displayed);
+            driver.FindElement(By.Id("btnCookie")).Click();
+            wait.Until((drv) => driver.FindElement(By.CssSelector("#pausa-card")).Displayed);
+            driver.FindElement(By.CssSelector("#pausa-card")).Click();
+        }
+
+        public void setISearchForTheCEP(string cep)
+        {
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+
+            driver.FindElement(By.CssSelector("#relaxation")).SendKeys(cep);
+            wait.Until((drv) => driver.FindElement(By.Id("relaxation")).Displayed);
+            driver.FindElement(By.CssSelector("div.card.card-mais-acessados.ordem:nth-child(4) form:nth-child(1) div.campo:nth-child(3) > button.bt-link-ic:nth-child(2)")).Click();
+        }
+
+        public void validateIConfirmThatTheCEPDoesNotExist()
+        {
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+
+            driver.SwitchTo().Window(driver.WindowHandles.Last());
+            wait.Until((drv) => driver.FindElement(By.CssSelector("h6:nth-child(1)")).Displayed);
+            Assert.AreEqual("Dados não encontrado", driver.FindElement(By.CssSelector("h6:nth-child(1)")).Text);
+        }
+
+        public void setISearchForTheTrackingCode(string trackingCode)
+        {
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+
+            wait.Until((drv) => driver.FindElement(By.CssSelector("#objetos")).Displayed);
+            driver.FindElement(By.CssSelector("#objetos")).SendKeys(trackingCode);
+            driver.FindElement(By.CssSelector("div.card.card-mais-acessados.ordem:nth-child(1) form:nth-child(1) div.campo:nth-child(3) > button.bt-link-ic:nth-child(2)")).Click();
+        }
+
+        public void validateIConfirmThatTheCodeIsNotCorrect()
+        {
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+
+            driver.SwitchTo().Window(driver.WindowHandles.Last());
+            wait.Until((drv) => driver.FindElement(By.CssSelector("h3:nth-child(1)")).Displayed);
+            Assert.AreEqual("Rastreamento", driver.FindElement(By.CssSelector("h3:nth-child(1)")).Text);
+        }
+
+        public void validateIConfirmThatTheResultIs(string expectedResult)
+        {
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+
+            driver.SwitchTo().Window(driver.WindowHandles.Last());
+            wait.Until((drv) => driver.FindElement(By.CssSelector("td:nth-child(1)")).Displayed);
+            Assert.AreEqual(expectedResult, driver.FindElement(By.CssSelector("td:nth-child(1)")).Text);
+        }
+    }
+}
---
+++ bin/Debug/net6.0/StepDefinitions/CorreiosStep.cs
@@ -0,0 +1,73 @@
+using System.Threading;
+using CorreiosSpecFlow.Actions;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using TechTalk.SpecFlow;
+
+namespace CorreiosSpecFlow.Steps
+{
+    [Binding]
+    public class CorreiosStep
+    {
+        private IWebDriver driver;
+
+        [BeforeScenario]
+        public void Setup()
+        {
+            driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+        }
+
+        [AfterScenario]
+        public void Teardown()
+        {
+            driver.Quit();
+        }
+
+        [Given(@"I am on the Correios website")]
+        public void GivenIAmOnTheCorreiosWebsite()
+        {
+            CorreiosAction correiosAction = new CorreiosAction(driver);
+
+            driver.Navigate().GoToUrl("https://www.correios.com.br/");
+            correiosAction.clickCookieSiteCorreiosAction();
+        }
+
+        [When(@"I search for the CEP (.*)")]
+        public void WhenISearchForTheCEP(string cep)
+        {
+            CorreiosAction correiosAction = new CorreiosAction(driver);
+            correiosAction.setISearchForTheCEP(cep);
+        }
+
+        [Then(@"I confirm that the CEP does not exist")]
+        public void ThenIConfirmThatTheCEPDoesNotExist()
+        {
+            CorreiosAction correiosAction = new CorreiosAction(driver);
+            correiosAction.validateIConfirmThatTheCEPDoesNotExist();
+        }
+
+        [When(@"I search for the tracking code (.*)")]
+        public void WhenISearchForTheTrackingCode(string trackingCode)
+        {
+            CorreiosAction correiosAction = new CorreiosAction(driver);
+            correiosAction.setISearchForTheTrackingCode(trackingCode);
+        }
+
+        [Then(@"I confirm that the code is not correct")]
+        public void ThenIConfirmThatTheCodeIsNotCorrect()
+        {
+            CorreiosAction correiosAction = new CorreiosAction(driver);
+            correiosAction.validateIConfirmThatTheCodeIsNotCorrect();
+        }
+
+        [Then(@"I confirm that the result is ""(.*)""")]
+        public void ThenIConfirmThatTheResultIs(string expectedResult)
+        {
+            CorreiosAction correiosAction = new CorreiosAction(driver);
+            correiosAction.validateIConfirmThatTheResultIs(expectedResult);
+        }
+    }
+}
